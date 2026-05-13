fbd249964bb798511673ec08afce0d9af03368da
Trong Thanh <nguyentrongthanh672@gmail.com>
2026-05-13 22:50:04 +0700

    .

---
+++ utils/base.py
@@ -0,0 +1,21 @@
+class SegmentationBase:
+    def __init__(self):
+        pass
+
+    def run(image):
+        """
+            Trả về ảnh đã được phân đoạn
+        """
+        pass
+    
+    def run_batch(images):
+        """
+            Trả về list ảnh đã được phân đoạn
+        """
+        pass
+    
+    def step_by_step(image):
+        """
+            Trả về list ảnh đã được phân đoạn theo từng bước và list tên của nó.
+        """
+        pass
